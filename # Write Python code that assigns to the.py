f8ac375eed18c80070a60a75161c4fa6fd7846cf
Vuk Djukich <vuk.djukich@vuchinasystems.com>
2015-01-08 22:44:45 -0800

    Test

---
+++ # Write Python code that assigns to the.py
@@ -0,0 +1,43 @@
+# Write Python code that assigns to the 
+# variable url a string that is the value 
+# of the first URL that appears in a link 
+# tag in the string page.
+# Your code should print http://udacity.com
+# Make sure that if page were changed to
+# page = '<a href="http://udacity.com">Hello world</a>'
+# that your code still prints the same thing.
+
+# page = contents of a web page
+page =('<div id="top_bin"><div id="top_content" class="width960">'
+'<div class="udacity float-left"><a href="http://udacity.com">')
+start_link = page.find('<a href=')
+
+start_link = page.find('<a href=')
+start_quote = page.find('"', start_link)
+end_quote = page.find('"', start_quote+1)
+url = page[start_quote+1:end_quote]
+print url
+page = page[end_quote:]
+start_link = page.find('<a href=')
+start_quote = page.find('"', start_link)
+end_quote = page.find('"', start_quote+1)
+url = page[start_quote+1:end_quote]
+print url
+page = page[end_quote:]
+start_link = page.find('<a href=')
+start_quote = page.find('"', start_link)
+end_quote = page.find('"', start_quote+1)
+url = page[start_quote+1:end_quote]
+print url
+
+
+
+#Procedural abstraction ----->
+
+def function():
+	pass
+
+def <name>(<parameters>):
+
+# The inputs for the procedure get_next_target is string giving contents of the rest of the web page.
+
